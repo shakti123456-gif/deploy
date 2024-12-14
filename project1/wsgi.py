05b4e349f75e11a6edbb840be2ba6249885acd6d
shakti <shakti.johnnette@gmail.com>
2024-12-14 09:17:58 +0530

    fix issue

--- project1/wsgi.py
+++ project1/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
+# print("t his is problem")
 
 application = get_wsgi_application()
