cc41c7d6f566a6e081c2c08af9bc637f759e5403
shakti <shakti.johnnette@gmail.com>
2024-12-27 00:26:55 +0530

    fix  this is issue

--- ReactjsBrowser/apps.py
+++ ReactjsBrowser/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ReactjsbrowserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ReactjsBrowser'
+
+
+class teksystem:
+    print("this is not working problem ")
